bce17f65375588b40506c8e144105dee6a26b7b5
Shema N. Valentin <shemavalentin@gmail.com>
2025-05-01 16:17:15 +0200

    Restaurant info and screens

--- App.js
+++ App.js
@@ -1,27 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StatusBar,
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  View,
-  Platform,
-} from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text> Search</Text>
-        </View>
-
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text> List </Text>
-        </View>
-      </SafeAreaView>
-      <StatusBar style="auto" />
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
