9998ad21fb6fddefa6acf49449b08b3fb180f687
Shema N. Valentin <shemavalentin@gmail.com>
2025-05-06 13:01:56 +0200

    Maintaining constistence in Themes

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,31 @@
+export const colors = {
+  brand: {
+    primary: "#2182BD",
+    secondary: "5282BD",
+    muted: "C6DAF7",
+  },
+
+  ui: {
+    primary: "#262626",
+    secondary: "#757575",
+    tertiary: "#F1F1F1",
+    quarternary: "#FFFFFF",
+    disabled: "#DEDEDE",
+    error: "#D0421B",
+    success: "#138000",
+  },
+
+  bg: {
+    primary: "#F5F5F5",
+    secondary: "#4863A0",
+  },
+
+  text: {
+    primary: "#262626",
+    secondary: "#757575",
+    disabled: "#9C9C9C",
+    inverse: "#FFFFFF",
+    error: "#D0421B",
+    success: "#138000",
+  },
+};
