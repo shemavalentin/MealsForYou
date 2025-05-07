244a7666ef49b40e18195a03d019529d112a36e2
Shema N. Valentin <shemavalentin@gmail.com>
2025-05-07 15:30:12 +0200

    Implemented Rating SVG stars

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+// Importing the asset that contains the svg properties
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -27,6 +31,14 @@ const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+// Making Stats to display on row direction wheter to be on column
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   // Using destructuring to take properties we need that are contained by restaurant(an object)
   const {
@@ -41,11 +53,25 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant;
 
+  // Now we need to render the star 4 times or five as on the rating variable. we need to iterate over rating variable
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  console.log(ratingArray);
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
+
+        {/*Wraping the rating in Rating component to be on row*/}
+        <Rating>
+          {/* Iteratin over the rating number to display number of starts */}
+          {/* Adding xml property  and width and height*/}
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
+
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
