86b3d3bb4ce77293a1730bf142a3a8f35dabdfff
Shema N. Valentin <shemavalentin@gmail.com>
2025-05-26 15:35:32 +0200

    Created Favourite context

---
+++ src/services/favourites/favourites.context.js
@@ -0,0 +1,42 @@
+/* 
+This component is going to be storing our favirites
+*/
+
+import React, { createContext, useState } from "react";
+
+export const FavouritesContext = createContext();
+
+// export the FavouritesContextProvider function that is going to
+// to wrap the react tree or the app.js in order for us to be able to
+// supply the favourites to the tree of react.
+
+export const FavouritesContextProvider = ({ children }) => {
+  const [favourites, setFavourites] = useState([]);
+
+  // Each time a user decides that they want to add a favorite we need to set up something to add and remove favourite
+  const add = (restauranat) => {
+    setFavourites([...favourites, restauranat]);
+  };
+
+  const remove = (restaurant) => {
+    const newFavourites = favourites.filter(
+      (x) => x.placeId !== restaurant.placeId
+    );
+
+    setFavourites(newFavourites);
+  };
+  return (
+    <FavouritesContext.Provider
+      // Providing our provider the value that it is going to provide externally and that value is important because it is the shape that is going to go to the outside of the component
+      value={{
+        favourites,
+        addToFavourites: add,
+        removeFromFavourites: remove,
+      }}
+    >
+      {/* Now inside here we are going to render our children */}
+
+      {children}
+    </FavouritesContext.Provider>
+  );
+};
