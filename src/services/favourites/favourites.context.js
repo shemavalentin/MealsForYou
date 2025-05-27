8bdbc56b4826d98dc8d2885efd5309095a0e0860
Shema N. Valentin <shemavalentin@gmail.com>
2025-05-27 16:36:05 +0200

    Storing Favourites on the local storage

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -2,7 +2,8 @@
 This component is going to be storing our favirites
 */
 
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
@@ -12,6 +13,31 @@ export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  // Saving Favourites in the cash
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      // the @favourites is the storage key here
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (e) {
+      console.log("Error storing", e);
+    }
+  };
+
+  // Reading the Favourites from the storage
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favourites");
+      if (value !== null) {
+        // value previously stored, then the JSON should perse it
+        setFavourites(JSON.parse(value));
+      }
+    } catch (e) {
+      // error reading value
+      console.log("Error loading", e);
+    }
+  };
 
   // Each time a user decides that they want to add a favorite we need to set up something to add and remove favourite
   const add = (restauranat) => {
@@ -25,6 +51,16 @@ export const FavouritesContextProvider = ({ children }) => {
 
     setFavourites(newFavourites);
   };
+
+  // Let's now on the very first mount of the context, make sure that we load the initial favourites.
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  // Let's now listen on Favourites and whenever there is any change on favourites, we store it
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
   return (
     <FavouritesContext.Provider
       // Providing our provider the value that it is going to provide externally and that value is important because it is the shape that is going to go to the outside of the component
