3f2c7bfc2c916c9d0da48de93364df34d8a9d365
Shema N. Valentin <shemavalentin@gmail.com>
2025-06-04 10:06:58 +0200

    Firebase Installation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "react-native-gesture-handler";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import { initializeApp } from "firebase/app";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -17,6 +18,18 @@ import { Navigation } from "./src/infrastructure/navigation/index";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDuZsXE10A0-9cy7wIYxyVg3dApf5_o258",
+  authDomain: "mealsforyou-d1a77.firebaseapp.com",
+  projectId: "mealsforyou-d1a77",
+  storageBucket: "mealsforyou-d1a77.firebasestorage.app",
+  messagingSenderId: "595776631693",
+  appId: "1:595776631693:web:d9c4c89a0cc16772a903c4",
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
