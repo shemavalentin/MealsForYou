76c21ac34af1778252ececcb45b4e01f602c804f
Shema N. Valentin <shemavalentin@gmail.com>
2025-06-12 15:53:59 +0200

    Added Avatar and user email in settings screen

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,23 +1,43 @@
 import React, { useContext } from "react";
-import { List } from "react-native-paper";
+import styled from "styled-components/native";
+import { List, Avatar } from "react-native-paper";
 
+import { Text } from "../../../components/typography/text.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
+// beautifying the screen settings
+
+const SettingItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+// Sizing the Avatar
+const AvatarContainer = styled.View`
+  align-items: center;
+`;
+
 export const SettingsScreen = ({ navigation }) => {
-  const { onLogout } = useContext(AuthenticationContext);
+  const { onLogout, user } = useContext(AuthenticationContext);
+  console.log("The loged in User is:", user);
   return (
     <SafeArea>
+      <AvatarContainer>
+        <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        <Spacer position="top" size="large">
+          <Text variant="caption">{user.user.email}</Text>
+        </Spacer>
+      </AvatarContainer>
+
       <List.Section>
-        <List.Item
-          style={{ padding: 16 }}
+        <SettingItem
           title="Favourites"
           description="View your favourites"
           left={(props) => <List.Icon {...props} color="black" icon="heart" />}
           onPress={() => navigation.navigate("Favourites")}
         />
-        <List.Item
-          style={{ padding: 16 }}
+        <SettingItem
           title="Logout"
           left={(props) => <List.Icon {...props} color="black" icon="door" />}
           onPress={onLogout}
