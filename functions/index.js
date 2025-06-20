c20c74a9fdad30cc95b2b3b72981de7188b68190
Shema N. Valentin <shemavalentin@gmail.com>
2025-06-20 09:58:57 +0200

    Location Mock

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const { geocodeRequest } = require("./geocode");
+const { placesRequest } = require("./places");
 
 exports.geocode = onRequest((request, response) => {
   geocodeRequest(request, response);
+});
+
+exports.placesNearby = onRequest((request, response) => {
+  placesRequest(request, response);
 });
