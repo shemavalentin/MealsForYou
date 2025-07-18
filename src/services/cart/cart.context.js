e55282168f8a59e66ba7fbeda107d878e7ec7e87
Shema N. Valentin <shemavalentin@gmail.com>
2025-07-18 13:06:58 +0200

    Adding the Checkout UI

--- src/services/cart/cart.context.js
+++ src/services/cart/cart.context.js
@@ -13,6 +13,25 @@ export const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [restaurant, setRestaurant] = useState(null);
 
+  // Setting a state to calculate the total
+  const [sum, setSum] = useState(0);
+
+  // Run this useEffect when the user cart changes
+
+  useEffect(() => {
+    if (!cart.length) {
+      setSum(0);
+      return;
+    }
+
+    // Otherwise do the summation
+
+    const newSum = cart.reduce((acc, { price }) => {
+      return (acc += price);
+    }, 0);
+    setSum(newSum);
+  }, [cart]);
+
   // Building the function that will add and remove from the cart
   const add = (item, rst) => {
     // rst here is short for restaurant
@@ -38,6 +57,7 @@ export const CartContextProvider = ({ children }) => {
         clearCart: clear,
         restaurant,
         cart,
+        sum,
       }}
     >
       {/* include children to render out everything subsquesntly */}
